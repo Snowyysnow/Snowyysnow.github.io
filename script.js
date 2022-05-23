9653b57031743d3177b72f2b1cd17fccaa901a7f
Snowyysnow <93723115+Snowyysnow@users.noreply.github.com>
2022-05-23 11:45:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+// create variables
+var time=45; //how much time they get
+var timesup=0;
+var started=0;
+
+// +,-,x,or /
+var operator;
+
+var rightAnswer;
+
+// start countdown
+function CountDown() {
+if(time>0)
+	{
+	document.math.timer.value=time;
+	time=time-1;
+	var gameTimer=setTimeout("CountDown()", 1000)}
+	else if (time==0)
+		{document.math.timer.value="0";
+		timesup=1;
+		alert('Time\'s Up!');
+		document.math.firstnum.value="";
+		document.math.operator.value="";
+		document.math.secondnum.value="";
+		document.math.answer.value="";
+	}
+}
+
+// Start the game
+function startgame(){
+	if (started!=0){
+		alert('You\'ve Already Started!');}
+	else
+	{
+		started=1;
+		CountDown();
+		getProb();
+	}
+}
+
+
+//generate a random number between 2 intergers
+// return the random number
+function randnum(min,max)
+{
+var num=Math.round(Math.random()*(max-min))+min;
+return num;
+}
+
+
+// gernerate the question
+// populate the form forelds
+// generate the answer 
+
+function getProb(){	
+// select operand
+// use our randnum function defined earlier
+operator = randnum(1,4);
+
+// operator +
+if (operator=="1"){
+	document.math.operator.value="+";
+	var choose1=randnum(0,50);
+	var choose2=randnum(0,50);
+	document.math.firstnum.value=choose1;
+	document.math.secondnum.value=choose2;
+	rightAnswer=choose1 + choose2;
+}
+
+// operator -
+if (operator=="2"){
+	document.math.operator.value="-";
+	var choose2=randnum(0,50);
+	var choose1=randnum(choose2,50);
+	document.math.firstnum.value=choose1;
+	document.math.secondnum.value=choose2;
+	rightAnswer=choose1 -  choose2;
+}
+
+
+// operator *
+if (operator=="3"){
+	document.math.operator.value="x";
+	var choose1=randnum(0,10);
+	var choose2=randnum(0,10);
+	document.math.firstnum.value=choose1;
+	document.math.secondnum.value=choose2;
+	rightAnswer=choose1 * choose2;
+}
+
+// operator /
+if (operator=="4"){
+	document.math.operator.value="/";
+	var choose2=randnum(1,10);
+	var choose1=choose2 * randnum(0,10);
+	document.math.firstnum.value=choose1;
+	document.math.secondnum.value=choose2;
+	rightAnswer=choose1 /  choose2;
+	}
+}
+
+
+
+// check your answer against the answer to the question
+function answerit(){
+	// has the gane started
+	if (started==0)	{
+		alert('You Must Click The Button Labeled \'Start\'!');}
+	else {
+	// is time up?	
+	if (timesup!=0)	{
+		alert('Time Ran Out!');}
+	else{
+
+		// assing user answer and user points to variables
+		var userAnswer=eval(document.math.answer.value);
+		var userPoints=eval(document.math.points.value);
+		
+
+			// has the use entered ann answer
+			if (userAnswer==null){
+				// no
+				alert('Put Your Answer In The Box To The Left Of The Button Labeled \'Answer\'!');
+				document.math.answer.select();
+				}
+				//yes
+				else{
+					// is their answer right
+					if (userAnswer == rightAnswer){
+					alert('Right');
+					theirpoints++;
+					document.math.points.value=userPoints;
+					}
+					else{
+						// no, it's wrong
+						alert(userAnswer + " is wrong!\n\n" + rightAnswer + " is correct!")}
+						document.math.answer.select();
+						getProb();
+						}
+					}
+				}
+	}
